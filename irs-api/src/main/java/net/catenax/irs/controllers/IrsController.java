5e65b80fb06ac7ce414bcf27bd70c74bb6ef4bed
ext.ooyeneye <Oladapo.Oyeneye@partner.doubleslash.de>
2022-03-15 07:56:57 +0100

    Initial git-action commit

--- irs-api/src/main/java/net/catenax/irs/controllers/IrsController.java
+++ irs-api/src/main/java/net/catenax/irs/controllers/IrsController.java
@@ -9,6 +9,7 @@
 //
 package net.catenax.irs.controllers;
 
+import edu.umd.cs.findbugs.annotations.SuppressWarnings;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
