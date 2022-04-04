20f84d9104b6515d62d12f8ee6aaf55ea93a81b8
Jan Kreutzfeld <jan.kreutzfeld@doubleslash.de>
2022-04-04 17:12:03 +0200

    feat(consumer):[TRI-309] add expiration rule for blob entries

--- connector/irs-connector-consumer/src/main/java/net/catenax/irs/connector/consumer/persistence/MinioBlobPersistence.java
+++ connector/irs-connector-consumer/src/main/java/net/catenax/irs/connector/consumer/persistence/MinioBlobPersistence.java
@@ -14,6 +14,7 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
 
 import io.minio.BucketExistsArgs;
 import io.minio.GetObjectArgs;
@@ -21,18 +22,27 @@ import io.minio.GetObjectResponse;
 import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
+import io.minio.SetBucketLifecycleArgs;
 import io.minio.errors.ErrorResponseException;
 import io.minio.errors.InsufficientDataException;
 import io.minio.errors.InternalException;
 import io.minio.errors.InvalidResponseException;
 import io.minio.errors.ServerException;
 import io.minio.errors.XmlParserException;
+import io.minio.messages.Expiration;
+import io.minio.messages.LifecycleConfiguration;
+import io.minio.messages.LifecycleRule;
+import io.minio.messages.ResponseDate;
+import io.minio.messages.RuleFilter;
+import io.minio.messages.Status;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * BlobPersistence implementation using the min.io library
  */
 public class MinioBlobPersistence implements BlobPersistence {
 
+    private static final Integer EXPIRE_AFTER_DAYS = 7;
     private final MinioClient minioClient;
     private final String bucketName;
 
@@ -49,9 +59,27 @@ public class MinioBlobPersistence implements BlobPersistence {
             if (!minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build())) {
                 minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
             }
+
+            setExpirationLifecycle(bucketName);
         } catch (ServerException | InsufficientDataException | ErrorResponseException | IOException | NoSuchAlgorithmException | InvalidKeyException | InvalidResponseException | XmlParserException | InternalException e) {
             throw new BlobPersistenceException("Encountered error while trying to create min.io client", e);
         }
+    }
+
+    private void setExpirationLifecycle(final String bucketName)
+            throws ErrorResponseException, InsufficientDataException, InternalException, InvalidKeyException,
+            InvalidResponseException, IOException, NoSuchAlgorithmException, ServerException, XmlParserException {
+        final Expiration expiration = new Expiration((ResponseDate) null, EXPIRE_AFTER_DAYS, false);
+        final LifecycleRule rule = createExpirationRule(expiration);
+        final LifecycleConfiguration lifecycleConfig = new LifecycleConfiguration(List.of(rule));
+        minioClient.setBucketLifecycle(
+                SetBucketLifecycleArgs.builder().bucket(bucketName).config(lifecycleConfig).build());
+    }
+
+    @NotNull
+    private LifecycleRule createExpirationRule(final Expiration expiration) {
+        return new LifecycleRule(Status.ENABLED, null, expiration, new RuleFilter(""), null, null,
+                null, null);
     }
 
     @Override
@@ -72,8 +100,7 @@ public class MinioBlobPersistence implements BlobPersistence {
     public byte[] getBlob(final String sourceBlobName) throws BlobPersistenceException {
         final GetObjectResponse response;
         try {
-            response = minioClient.getObject(
-                    GetObjectArgs.builder().bucket(bucketName).object(sourceBlobName).build());
+            response = minioClient.getObject(GetObjectArgs.builder().bucket(bucketName).object(sourceBlobName).build());
         } catch (ServerException | InsufficientDataException | ErrorResponseException | IOException | NoSuchAlgorithmException | InvalidKeyException | InvalidResponseException | XmlParserException | InternalException e) {
             throw new BlobPersistenceException("Encountered error while trying to load blob", e);
         }
