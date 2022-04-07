143ca380bed117a4a58e886f0b64b2aa8144bdf1
ds-jhartmann <jaro.hartmann@doubleslash.de>
2022-04-07 15:53:16 +0200

    refactor(irs-api):[TRI-281] Change Test names

--- irs-api/src/test/java/net/catenax/irs/aaswrapper/registry/domain/AssetAdministrationShellTestdataCreatorTest.java
+++ irs-api/src/test/java/net/catenax/irs/aaswrapper/registry/domain/AssetAdministrationShellTestdataCreatorTest.java
@@ -14,7 +14,7 @@ class AssetAdministrationShellTestdataCreatorTest {
     }
 
     @Test
-    void testCreateDummyAssetAdministrationShellDescriptorForId() {
+    void shouldReturnAssetAdministrationShellDescriptorWhenRequestingWithCatenaXId() {
         final String catenaXId = "8a61c8db-561e-4db0-84ec-a693fc5ffdf6";
 
         final AssetAdministrationShellDescriptor aasDescriptor = assetAdministrationShellTestdataCreator.createAASShellDescriptorForIdFromTestData(
