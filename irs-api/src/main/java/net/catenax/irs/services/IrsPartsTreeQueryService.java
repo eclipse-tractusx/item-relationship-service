019e0f99f5197945568110215b1c9f2e8633989a
Jan Kreutzfeld <jan.kreutzfeld@doubleslash.de>
2022-04-08 12:46:51 +0200

    feat(deploy):[TRI-296] Fix uuid regex, implement job starter

--- irs-api/src/main/java/net/catenax/irs/services/IrsPartsTreeQueryService.java
+++ irs-api/src/main/java/net/catenax/irs/services/IrsPartsTreeQueryService.java
@@ -10,11 +10,18 @@
 package net.catenax.irs.services;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.UUID;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.catenax.irs.aaswrapper.job.AASRecursiveJobHandler;
+import net.catenax.irs.aaswrapper.job.AASTransferProcess;
+import net.catenax.irs.aaswrapper.job.ItemDataRequest;
+import net.catenax.irs.connector.job.JobInitiateResponse;
+import net.catenax.irs.connector.job.JobOrchestrator;
 import net.catenax.irs.component.Job;
 import net.catenax.irs.component.JobHandle;
 import net.catenax.irs.component.Jobs;
@@ -31,10 +38,14 @@ import org.springframework.stereotype.Service;
 @ExcludeFromCodeCoverageGeneratedReport
 public class IrsPartsTreeQueryService implements IIrsPartTreeQueryService {
 
+    private final JobOrchestrator<ItemDataRequest, AASTransferProcess> orchestrator;
+
     @Override
     public JobHandle registerItemJob(final @NonNull IrsPartsTreeRequest request) {
-        throw new UnsupportedOperationException();
-        //return null;
+        final JobInitiateResponse jobInitiateResponse = orchestrator.startJob(
+                Map.of(AASRecursiveJobHandler.ROOT_ITEM_ID_KEY, request.getGlobalAssetId()));
+        final String jobId = jobInitiateResponse.getJobId();
+        return JobHandle.builder().jobId(UUID.fromString(jobId)).build();
     }
 
     @Override
