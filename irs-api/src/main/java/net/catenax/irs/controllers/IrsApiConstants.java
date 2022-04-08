11cc920f76e4dec0bad531809a822485a6e7870f
Jan Kreutzfeld <jan.kreutzfeld@doubleslash.de>
2022-04-08 15:16:06 +0200

    feat(deploy):[TRI-296] Fix expected UUID length

--- irs-api/src/main/java/net/catenax/irs/controllers/IrsApiConstants.java
+++ irs-api/src/main/java/net/catenax/irs/controllers/IrsApiConstants.java
@@ -20,5 +20,5 @@ public final class IrsApiConstants {
     public static final String GLOBAL_ASSET_ID_REGEX = "^urn:uuid:[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$";
     public static final int UUID_SIZE = 36;
     public static final int URN_PREFIX_SIZE = 9;
-    public static final int JOB_ID_SIZE = UUID_SIZE + URN_PREFIX_SIZE;
+    public static final int JOB_ID_SIZE = UUID_SIZE;
 }
