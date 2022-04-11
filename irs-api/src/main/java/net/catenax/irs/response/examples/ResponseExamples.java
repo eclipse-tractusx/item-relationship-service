cb3e33372013b99fe102eb475d72b7fd5abd6e4d
mk <martin.kanal@doubleslash.de>
2022-04-11 10:40:45 +0200

    fix(model):[TRI-290] Fix Checkstyle errors

--- irs-api/src/main/java/net/catenax/irs/response/examples/ResponseExamples.java
+++ irs-api/src/main/java/net/catenax/irs/response/examples/ResponseExamples.java
@@ -1,7 +1,19 @@
+//
+// Copyright (c) 2021 Copyright Holder (Catena-X Consortium)
+//
+// See the AUTHORS file(s) distributed with this work for additional
+// information regarding authorship.
+//
+// See the LICENSE file(s) distributed with this work for
+// additional information regarding license terms.
+//
 package net.catenax.irs.response.examples;
 
+/**
+ * Response example for irs api
+ */
 public class ResponseExamples {
-   public static final String JobHandleExample = "{\"jobId\": \"6c311d29-5753-46d4-b32c-19b918ea93b0\"}";
+    public static final String JOB_HANLDE_EXAMPLE = "{\"jobId\": \"6c311d29-5753-46d4-b32c-19b918ea93b0\"}";
 
-   public static final String ErrorResponse  = "{\"errors\": [\"TimeoutException\", \"ParsingException\"],\"message\": \"Some errors occured\",\"statusCode\": \"500\"}";
+    public static final String ERROR_RESPONSE  = "{\"errors\": [\"TimeoutException\", \"ParsingException\"],\"message\": \"Some errors occured\",\"statusCode\": \"500\"}";
 }
