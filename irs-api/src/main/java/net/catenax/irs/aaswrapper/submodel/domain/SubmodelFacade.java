e15dcc3f053d965a2a80423666a73e9bb6192a3b
dridis <saber.dridi@istos.com>
2022-04-13 13:10:04 +0200

    feature/TRI-337: Move retry method into the SubmodelFacade class and do the related adjustment.

--- irs-api/src/main/java/net/catenax/irs/aaswrapper/submodel/domain/SubmodelFacade.java
+++ irs-api/src/main/java/net/catenax/irs/aaswrapper/submodel/domain/SubmodelFacade.java
@@ -12,6 +12,7 @@ package net.catenax.irs.aaswrapper.submodel.domain;
 import java.util.HashSet;
 import java.util.Set;
 
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import net.catenax.irs.dto.AssemblyPartRelationshipDTO;
 import net.catenax.irs.dto.ChildDataDTO;
@@ -31,13 +32,33 @@ public class SubmodelFacade {
      * @return The Aspect Model for the given submodel
      */
     public AssemblyPartRelationshipDTO getSubmodel(final String submodelEndpointAddress) {
-        final AssemblyPartRelationship submodel = submodelClient.getSubmodel(submodelEndpointAddress, AssemblyPartRelationship.class);
+        final AssemblyPartRelationship submodel = submodelClient.getSubmodel(submodelEndpointAddress,
+                                                                             AssemblyPartRelationship.class);
         final Set<ChildDataDTO> childParts = new HashSet<>();
         submodel.getChildParts()
                 .forEach(childData -> childParts.add(ChildDataDTO.builder()
                                                                  .withChildCatenaXId(childData.getChildCatenaXId())
-                                                                 .withLifecycleContext(childData.getLifecycleContext().getValue())
+                                                                 .withLifecycleContext(
+                                                                         childData.getLifecycleContext().getValue())
                                                                  .build()));
-        return AssemblyPartRelationshipDTO.builder().withCatenaXId(submodel.getCatenaXId()).withChildParts(childParts).build();
+        return AssemblyPartRelationshipDTO.builder()
+                                          .withCatenaXId(submodel.getCatenaXId())
+                                          .withChildParts(childParts)
+                                          .build();
+    }
+
+    /**
+     * Retry the call getting a submodel from the remote
+     * aas wrapper service
+     *
+     * @param submodelEndpointAddress The URL to the submodel endpoint
+     * @param submodelClass           The Aspect Model for the given submodel
+     *                                like AssemblyPartRelationship
+     * @param <T>                     the generic Aspect Model
+     * @return the requested submodel
+     */
+    @Retry(name = "submodelRetryer")
+    public <T> T retrySubmodel(final String submodelEndpointAddress, final Class<T> submodelClass) {
+        return this.submodelClient.getSubmodel(submodelEndpointAddress, submodelClass);
     }
 }
--- irs-api/src/test/java/net/catenax/irs/aaswrapper/submodel/domain/SubmodelRetryerTest.java
+++ irs-api/src/test/java/net/catenax/irs/aaswrapper/submodel/domain/SubmodelRetryerTest.java
@@ -1,11 +1,5 @@
 package net.catenax.irs.aaswrapper.submodel.domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
-
 import io.github.resilience4j.retry.RetryRegistry;
 import net.catenax.irs.InMemoryBlobStore;
 import net.catenax.irs.persistence.BlobPersistence;
@@ -21,10 +15,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.client.HttpServerErrorException;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles(profiles = { "local", "test" })
 class SubmodelRetryerTest {
-    private SubmodelRetryer submodelRetryer;
+    private SubmodelFacade facade;
 
     @Mock
     private SubmodelClientImpl submodelClient;
@@ -37,13 +37,13 @@ class SubmodelRetryerTest {
         when(this.submodelClient.getSubmodel(anyString(), any())).thenThrow(
                 new HttpServerErrorException(HttpStatus.INTERNAL_SERVER_ERROR, "AASWrapper remote exception"));
 
-        this.submodelRetryer = new SubmodelRetryer(this.submodelClient);
+        this.facade = new SubmodelFacade(this.submodelClient);
     }
 
     @Test
     void submodelRetryerShouldRetryThreeTimes() {
         assertThatExceptionOfType(HttpServerErrorException.class).isThrownBy(
-                                                                         () -> this.submodelRetryer.retrySubmodel("aasWrapperEndpoint", AssemblyPartRelationship.class))
+                                                                         () -> this.facade.retrySubmodel("aasWrapperEndpoint", AssemblyPartRelationship.class))
                                                                  .withMessage("500 AASWrapper remote exception");
 
         final long attempts = this.retryRegistry.getDefaultConfig().getMaxAttempts();
