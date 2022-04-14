8b431a6f91452aa14342e5f43ca8b991813e8df1
mk <martin.kanal@doubleslash.de>
2022-04-14 09:22:27 +0200

    fix(controller):[TRI-326] removed not defined tag

--- irs-api/src/main/java/net/catenax/irs/controllers/IrsController.java
+++ irs-api/src/main/java/net/catenax/irs/controllers/IrsController.java
@@ -23,7 +23,6 @@ import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.catenax.irs.IrsApplication;
@@ -49,7 +48,6 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Application REST controller.
  */
-@Tag(name = "Item Relationship Service")
 @Slf4j
 @RestController
 @RequestMapping(IrsApplication.API_PREFIX)
