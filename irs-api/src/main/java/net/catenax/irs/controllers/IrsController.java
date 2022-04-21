69f76261410cbbb814a85a55f1f916887f7f58b0
dridis <saber.dridi@istos.com>
2022-04-21 10:12:45 +0200

    feature/TRI-342: @SuppressWarnings("PMD.TooManyMethods")

--- irs-api/src/main/java/net/catenax/irs/controllers/IrsController.java
+++ irs-api/src/main/java/net/catenax/irs/controllers/IrsController.java
@@ -40,7 +40,6 @@ import net.catenax.irs.dtos.ErrorResponse;
 import net.catenax.irs.services.IrsItemGraphQueryService;
 import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
