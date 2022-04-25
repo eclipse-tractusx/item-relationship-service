7cd0b082d168eec18ee4b2b995c5efa34d35d813
Jan Kreutzfeld <jan.kreutzfeld@doubleslash.de>
2022-04-25 12:29:35 +0200

    chore(cleanup):[TRI-274] Remove outdated stuff

--- integration-tests/src/test/java/net/catenax/irs/systemtest/SystemTestsBase.java
+++ integration-tests/src/test/java/net/catenax/irs/systemtest/SystemTestsBase.java
@@ -11,16 +11,9 @@ import org.junit.jupiter.api.Disabled;
 public class SystemTestsBase {
 
     protected static final String PATH_BY_VIN = "/api/v0.1/vins/{vin}/partsTree";
-    protected static final String PATH_BY_IDS = "/api/v0.1/parts/{oneIDManufacturer}/{objectIDManufacturer}/partsTree";
-    protected static final String PATH_UPDATE_RELATIONSHIPS = "/broker-proxy/v0.1/partRelationshipUpdateList";
-    protected static final String ONE_ID_MANUFACTURER = "oneIDManufacturer";
-    protected static final String OBJECT_ID_MANUFACTURER = "objectIDManufacturer";
     protected static final String SAMPLE_VIN = "YS3DD78N4X7055320";
     protected static final String VIN = "vin";
     protected static final String VIEW = "view";
-    protected static final String ASPECT = "aspect";
-    protected static final String ASPECT_MATERIAL = "MATERIAL";
-    protected static final String DEPTH = "depth";
 
     protected RequestSpecification getRequestSpecification() {
         var specificationBuilder = new RequestSpecBuilder();
