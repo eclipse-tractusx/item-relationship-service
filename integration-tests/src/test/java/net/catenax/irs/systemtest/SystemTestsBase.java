c79b7cc6bc1f5ff12297eaffe9ebfb30abb138de
Jan Kreutzfeld <jan.kreutzfeld@doubleslash.de>
2022-04-25 13:29:28 +0200

    chore(cleanup):[TRI-274] Remove outdated constants

--- integration-tests/src/test/java/net/catenax/irs/systemtest/SystemTestsBase.java
+++ integration-tests/src/test/java/net/catenax/irs/systemtest/SystemTestsBase.java
@@ -12,9 +12,6 @@ public class SystemTestsBase {
 
     protected static final String PATH_CREATE_JOB = "/irs/jobs";
     protected static final String PATH_GET_JOB = "/irs/jobs/{jobId}";
-    protected static final String SAMPLE_VIN = "YS3DD78N4X7055320";
-    protected static final String VIN = "vin";
-    protected static final String VIEW = "view";
 
     protected RequestSpecification getRequestSpecification() {
         var specificationBuilder = new RequestSpecBuilder();
