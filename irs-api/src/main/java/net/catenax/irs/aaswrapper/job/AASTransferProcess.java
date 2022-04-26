0a496a6a51412453526ba8e4ba82f2c0fa5846ba
Krzysztof Massalski (Extern) <Krzysztof.Massalski@doubleslash.de>
2022-04-26 09:51:12 +0200

    feat(api):[TRI-339] - remove custom deserializer

--- irs-api/src/main/java/net/catenax/irs/aaswrapper/job/AASTransferProcess.java
+++ irs-api/src/main/java/net/catenax/irs/aaswrapper/job/AASTransferProcess.java
@@ -9,60 +9,31 @@
 //
 package net.catenax.irs.aaswrapper.job;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.ObjectCodec;
-import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 import net.catenax.irs.connector.job.TransferProcess;
 
 /**
  * Transfer Process for AAS Objects
  */
 @Getter
-@JsonDeserialize(using = AASTransferProcess.AASTransferProcessDeserializer.class)
-@RequiredArgsConstructor
+@NoArgsConstructor
 @AllArgsConstructor
 public class AASTransferProcess implements TransferProcess {
 
-    @Getter(AccessLevel.NONE)
-    private final String transferProcessId;
-
-    private final List<String> idsToProcess = new ArrayList<>();
+    @SuppressWarnings("PMD.ShortVariable")
+    private String id;
 
     private Integer depth;
+
+    private final List<String> idsToProcess = new ArrayList<>();
 
     public void addIdsToProcess(final List<String> childIds) {
         idsToProcess.addAll(childIds);
     }
 
-    @Override
-    public String getId() {
-        return transferProcessId;
-    }
-
-    /**
-     * Deserializer for AASTransferProcess
-     */
-    public static class AASTransferProcessDeserializer extends JsonDeserializer<AASTransferProcess> {
-
-        @Override
-        public AASTransferProcess deserialize(final JsonParser jsonParser,
-                final DeserializationContext deserializationContext) throws IOException {
-            final ObjectCodec codec = jsonParser.getCodec();
-            final JsonNode treeNode = codec.readTree(jsonParser);
-            final String idValue = treeNode.get("id").textValue();
-
-            return new AASTransferProcess(idValue);
-        }
-    }
 }
