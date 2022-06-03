53e6dab66615d3d3c4f87e87a63f6e8578cf3db9
ds-jhartmann <jaro.hartmann@doubleslash.de>
2022-06-03 13:58:00 +0200

    chore(codestyle):[TRI-497] Adjust comparison of generated api yaml and stored to ignore whitespaces

--- irs-api/src/test/java/net/catenax/irs/IrsApplicationTests.java
+++ irs-api/src/test/java/net/catenax/irs/IrsApplicationTests.java
@@ -51,15 +51,11 @@ class IrsApplicationTests {
     private JobOrchestrator<ItemDataRequest, AASTransferProcess> jobOrchestrator;
 
     @Test
-    void contextLoads() {
-    }
-
-    @Test
     void generatedOpenApiMatchesContract() throws Exception {
         final String generatedYaml =
                 this.restTemplate.getForObject("http://localhost:" + port + "/api/api-docs.yaml", String.class);
         final String fixedYaml = Files.readString(new File("../api/irs-v1.0.yaml").toPath(), UTF_8);
-        assertThat(generatedYaml).isEqualToNormalizingNewlines(fixedYaml);
+        assertThat(generatedYaml).isEqualToIgnoringWhitespace(fixedYaml);
     }
 
     @Test
