63d2a7b4e04476ebaee1ca109614f28e4ea3cca7
dridis <saber.dridi@istos.com>
2022-06-03 17:07:49 +0200

    feature/TRI-488: Readjust syntax of the relative path

--- integration-tests/src/test/java/net/catenax/irs/smoketest/ItemGraphSmokeTest.java
+++ integration-tests/src/test/java/net/catenax/irs/smoketest/ItemGraphSmokeTest.java
@@ -29,7 +29,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = {SmokeTestConfiguration.class})
