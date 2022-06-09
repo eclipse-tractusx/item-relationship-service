cc9fd25cc29ac93374af3e81edf026fe9076e957
Krzysztof Massalski (Extern) <Krzysztof.Massalski@doubleslash.de>
2022-06-09 12:26:26 +0200

    feat(impl):[TRI-507] direct call to registry, and rewrite urls to submodels

--- irs-api/src/test/java/net/catenax/irs/aaswrapper/submodel/domain/AASWrapperUriAddressRewritePolicyTest.java
+++ irs-api/src/test/java/net/catenax/irs/aaswrapper/submodel/domain/AASWrapperUriAddressRewritePolicyTest.java
@@ -43,7 +43,7 @@ class AASWrapperUriAddressRewritePolicyTest {
 
         final AASWrapperUriAddressRewritePolicy.AASWrapperUri aasWrapperUri = new AASWrapperUriAddressRewritePolicy.AASWrapperUri(endpointAddress);
 
-        assertThat(aasWrapperUri.getProviderConnectUrl()).isEqualTo("https://edc-ocp0900009.apps.c7von4sy.westeurope.aroapp.io/BPNL00000003B2OM");
+        assertThat(aasWrapperUri.getProviderConnectorUrl()).isEqualTo("https://edc-ocp0900009.apps.c7von4sy.westeurope.aroapp.io/BPNL00000003B2OM");
         assertThat(aasWrapperUri.getPath()).isEqualTo("/urn:uuid:5a7ab616-989f-46ae-bdf2-32027b9f6ee6-urn:uuid:31b614f5-ec14-4ed2-a509-e7b7780083e7/submodel");
         assertThat(aasWrapperUri.getQuery()).isEqualTo("content=value&extent=withBlobValue");
     }
