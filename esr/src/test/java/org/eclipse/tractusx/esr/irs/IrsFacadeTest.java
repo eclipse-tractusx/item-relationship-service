01596129f7959cdd9bd90255446f5011e8cc6b1b
Adam Bugajewski <adam.bugajewski@qualityminds.pl>
2022-09-14 09:32:20 +0200

    TRI-678 add unit tests for IRS Client

---
+++ esr/src/test/java/org/eclipse/tractusx/esr/irs/IrsFacadeTest.java
@@ -0,0 +1,51 @@
+package org.eclipse.tractusx.esr.irs;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class IrsFacadeTest {
+
+    @Mock
+    private IrsClient irsClient;
+
+    @InjectMocks
+    private IrsFacade irsFacade;
+
+    @Test
+    public void shouldFetchIrsResponseBasedOnInputs() {
+        // given
+        String globalAssetId = "global-asset-id";
+        String bomLifecycle = "asBuilt";
+        String token = "auth-token";
+        String jobId = "job-id";
+        IrsResponse expectedResponse = IrsResponse.builder()
+                .job(JobStatus.builder().jobState("COMPLETED").build())
+                .relationships(new ArrayList<>())
+                .shells(new ArrayList<>())
+                .build();
+
+        BDDMockito.given(irsClient.startJob(
+                IrsRequest.builder().globalAssetId(globalAssetId).bomLifecycle(bomLifecycle).build(),
+                token)
+        ).willReturn(StartJobResponse.builder().jobId(jobId).build());
+
+        BDDMockito.given(irsClient.getJobDetails(jobId, token)
+        ).willReturn(expectedResponse);
+
+        // when
+        IrsResponse actualResponse = irsFacade.getIrsResponse(globalAssetId, bomLifecycle, token);
+
+        // then
+        assertThat(actualResponse).isEqualTo(expectedResponse);
+    }
+
+}
---
+++ esr/src/test/java/org/eclipse/tractusx/esr/irs/JobIsRunningTest.java
@@ -0,0 +1,33 @@
+package org.eclipse.tractusx.esr.irs;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class JobIsRunningTest {
+
+    @Test
+    public void shouldRecognizeWhenJobIsRunning() {
+        // given
+        JobIsRunning jobIsRunning = new JobIsRunning();
+
+        // when
+        boolean result = jobIsRunning.test(IrsResponse.builder().job(JobStatus.builder().jobState("RUNNING").build()).build());
+
+        // then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void shouldRecognizeWhenJobIsCompleted() {
+        // given
+        JobIsRunning jobIsRunning = new JobIsRunning();
+
+        // when
+        boolean result = jobIsRunning.test(IrsResponse.builder().job(JobStatus.builder().jobState("COMPLETED").build()).build());
+
+        // then
+        assertThat(result).isFalse();
+    }
+
+}
