34798f9594ba9a97d694a85713f04e235e5bd0f6
Jaro Hartmann <jaro.hartmann@doubleslash.de>
2022-10-06 14:06:35 +0200

    fix(irs-api):[-] Fix KICS finding missing minimum and maximum values for numbers

--- irs-models/src/main/java/org/eclipse/tractusx/irs/component/JobParameter.java
+++ irs-models/src/main/java/org/eclipse/tractusx/irs/component/JobParameter.java
@@ -23,6 +23,9 @@ package org.eclipse.tractusx.irs.component;
 
 import java.util.List;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -51,6 +54,8 @@ public class JobParameter {
     private List<AspectType> aspects;
 
     @Schema(implementation = Integer.class)
+    @Min(0)
+    @Max(Integer.MAX_VALUE)
     private Integer depth;
 
     @Schema(implementation = Direction.class)
--- irs-models/src/main/java/org/eclipse/tractusx/irs/component/ProcessingError.java
+++ irs-models/src/main/java/org/eclipse/tractusx/irs/component/ProcessingError.java
@@ -23,8 +23,12 @@ package org.eclipse.tractusx.irs.component;
 
 import java.time.ZonedDateTime;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Value;
 
@@ -37,6 +41,10 @@ import lombok.Value;
 public class ProcessingError {
     private final String errorDetail;
     private final ZonedDateTime lastAttempt;
+
+    @Schema(implementation = Integer.class)
+    @Min(0)
+    @Max(Integer.MAX_VALUE)
     private int retryCounter;
 
     /**
