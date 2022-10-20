4726f1c81d7c53833c6a2cb6cd758a386c6b8177
ext.abulgakov <alexander.bulgakov@partner.doubleslash.de>
2022-10-20 15:25:21 +0200

    TRI-759: Fixed external-functions

--- api-tests/tavern_helpers.py
+++ api-tests/tavern_helpers.py
@@ -1,12 +1,40 @@
 # testing_utils.py
-def array_is_empty(response):
+def tombstones_are_empty(response):
+    """check array is empty"""
+    assert len(response.json().get("tombstones")) == 0
+
+
+def tombstones_are_not_empty(response):
+    """check array is empty"""
+    assert len(response.json().get("tombstones")) != 0
+
+    # try:
+    #     assert len(response.json().get("tombstones")) == 0
+    # except TypeError as e:
+    #     raise (
+    #         "Tried to match a pykwalify schema against a non-json response"
+    #     ) from e
+
+
+def relationships_are_empty(response):
     """check array is empty
     """
-    # assert len(response.json().get("tombstones")) != 0
+    assert len(response.json().get("relationships")) == 0
 
-    try:
-        assert len(response.json().get("tombstones")) == 0
-    except TypeError as e:
-        raise (
-            "Tried to match a pykwalify schema against a non-json response"
-        ) from e
+
+def relationships_are_not_empty(response):
+    """check array is empty
+    """
+    assert len(response.json().get("relationships")) != 0
+
+
+def submodels_are_empty(response):
+    """check array is empty
+    """
+    assert len(response.json().get("submodels")) == 0
+
+
+def submodels_are_not_empty(response):
+    """check array is empty
+    """
+    assert len(response.json().get("submodels")) != 0
