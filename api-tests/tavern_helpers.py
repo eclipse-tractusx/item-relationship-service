51b6f0164f4f2fbc57c2f7758f9c990726d52667
ext.abulgakov <alexander.bulgakov@partner.doubleslash.de>
2022-10-24 11:37:03 +0200

    TRI-759: Added tests for deprecated aspects and submodels-check

--- api-tests/tavern_helpers.py
+++ api-tests/tavern_helpers.py
@@ -7,45 +7,32 @@ def checkResponse(response):
 
 
 def tombstones_are_empty(response):
-    print (response.json().get("tombstones"));
-    print ("Check array is empty ", len(response.json().get("tombstones")))
+    print(response.json().get("tombstones"))
+    print("Check array is empty ", len(response.json().get("tombstones")))
     assert len(response.json().get("tombstones")) == 0
-    
 
 
 def tombstones_are_not_empty(response):
-
-    print (response.json().get("tombstones"));
-    print ("Check if tombstones are not empty number:", len(response.json().get("tombstones")))
+    print(response.json().get("tombstones"))
+    print("Check if tombstones are not empty number:", len(response.json().get("tombstones")))
     assert len(response.json().get("tombstones")) != 0
-
-    # try:
-    #     assert len(response.json().get("tombstones")) == 0
-    # except TypeError as e:
-    #     raise (
-    #         "Tried to match a pykwalify schema against a non-json response"
-    #     ) from e
 
 
 def relationships_are_empty(response):
-
-    print (response.json().get("relationships"));
-    print ("Check if relationships are empty", len(response.json().get("relationships")))
+    print(response.json().get("relationships"))
+    print("Check if relationships are empty", len(response.json().get("relationships")))
     assert len(response.json().get("relationships")) == 0
 
 
 def relationships_are_not_empty(response):
-
-    print (response.json().get("relationships"));
-    print ("Check if relationships are not empty", len(response.json().get("relationships")))
+    print(response.json().get("relationships"))
+    print("Check if relationships are not empty", len(response.json().get("relationships")))
     assert len(response.json().get("relationships")) != 0
 
 
 def submodels_are_empty(response):
-
     assert len(response.json().get("submodels")) == 0
 
 
 def submodels_are_not_empty(response):
-
     assert len(response.json().get("submodels")) != 0
