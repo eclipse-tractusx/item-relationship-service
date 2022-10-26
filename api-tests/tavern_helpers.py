3490bcaf9bdefa11803a72ba75ea39fca710b42a
ext.abulgakov <alexander.bulgakov@partner.doubleslash.de>
2022-10-26 12:52:23 +0200

    TRI-795: Fixed error check for wrong globalAssetIds and added new externalfunctions

--- api-tests/tavern_helpers.py
+++ api-tests/tavern_helpers.py
@@ -1,5 +1,6 @@
 # testing_utils.py
 
+
 def checkResponse(response):
     tombstones_are_not_empty(response)
     relationships_are_not_empty(response)
@@ -31,8 +32,19 @@ def relationships_are_not_empty(response):
 
 
 def submodels_are_empty(response):
+    print(response.json().get("submodels"))
+    print("Check if submodels are empty", len(response.json().get("submodels")))
     assert len(response.json().get("submodels")) == 0
 
 
 def submodels_are_not_empty(response):
+    print(response.json().get("submodels"))
+    print("Check if submodels are not empty", len(response.json().get("submodels")))
     assert len(response.json().get("submodels")) != 0
+
+
+def errors_for_globalAssetId_are_correct(response):
+    print(response.json().get("errors"))
+    error_list = response.json().get("errors")
+    assert 'globalAssetId:size must be between 45 and 45' in error_list
+    assert 'globalAssetId:must match \"^urn:uuid:[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$\"' in error_list
