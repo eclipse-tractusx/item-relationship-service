0abafa7312192feda8ad8e518b5482018c10ead1
Martin Maul <martin.maul@doubleslash.de>
2023-02-13 11:42:35 +0100

    chore(workflows):[TRI-1083] WIP added .puml diagram to .png image replacer with workflow

--- docs/src/diagram-replacer/replace.js
+++ docs/src/diagram-replacer/replace.js
@@ -1,93 +1,100 @@
 const fs = require("fs");
 const path = require("path");
-const { spawn } = require("child_process");
-const TARGET_PATH = "../../target/";
+const SOURCE_PATH = "../../target/";
+const TARGET_PATH = "./generated-adocs/";
 const IMAGES_PATH = "./extracted-images/";
 
-fs.readdirSync(TARGET_PATH).forEach((file) => {
-  fs.readFile(TARGET_PATH + file, "utf8", (err, data) => {
-    if (err) throw err;
+/**
+ * CAUTION: Images get not replaced if you specified an imagedir variable in the .adoc
+ */
 
-    let imageList = [];
-    // loop over images in extracted Image Directory of corresponding adoc
-    fs.readdirSync(IMAGES_PATH + path.parse(file).name + "/").forEach(
-      (image) => {
-        // add suffix _000 to the first png that is exported to  by plantuml.jar
-        if (path.parse(image).name === path.parse(file).name) {
-          fs.rename(
-            IMAGES_PATH + path.parse(file).name + "/" + image,
-            IMAGES_PATH +
-              path.parse(file).name +
-              "/" +
-              path.parse(image).name +
-              "_000.png",
-            function (err) {
-              if (err) {
-                console.log("Error trying to read images: Access failed or no Images found. Error:")
-                console.log(err);
-                
-              } 
-            }
+fs.readdirSync(SOURCE_PATH).forEach((file) => {
+  if (path.extname(file) === ".adoc") {
+    fs.readFile(SOURCE_PATH + file, "utf8", (err, data) => {
+      if (err) throw err;
+
+      let imageList = [];
+      // loop over images in extracted Image Directory of corresponding adoc
+      fs.readdirSync(IMAGES_PATH + path.parse(file).name + "/").forEach(
+        (image) => {
+          // add suffix _000 to the first png that is exported to  by plantuml.jar
+          if (path.parse(image).name === path.parse(file).name) {
+            fs.rename(
+              IMAGES_PATH + path.parse(file).name + "/" + image,
+              IMAGES_PATH +
+                path.parse(file).name +
+                "/" +
+                path.parse(image).name +
+                "_000.png",
+              function (err) {
+                if (err) {
+                  console.log(
+                    "Error trying to read images: Access failed or no Images found. Error:"
+                  );
+                  console.log(err);
+                }
+              }
+            );
+          }
+          imageList.push(image);
+        }
+      );
+
+      if (!imageList.length) {
+        console.log("No Images to replace.");
+        return;
+      }
+
+      let ImageDirectoryPathForAdoc = IMAGES_PATH + path.parse(file).name + "/";
+
+      const lines = data.split("\n");
+      let insidePlantUmlBlock = false;
+      const output = [];
+
+      for (let i = 0; i < lines.length; i++) {
+        // when line starts with plantuml tag replace with corresponding .PNG
+        if (lines[i].startsWith("[plantuml, target=")) {
+          const filename = lines[i].match(/target=(.*), format=svg/)[1];
+          output.push(
+            `image::${"." + ImageDirectoryPathForAdoc + imageList.shift()}[]`
+          );
+        } else if (lines[i].startsWith("@startuml")) {
+          insidePlantUmlBlock = true;
+        } else if (lines[i].startsWith("@enduml")) {
+          insidePlantUmlBlock = false;
+          continue;
+        } else if (insidePlantUmlBlock) {
+          continue;
+        } else {
+          output.push(lines[i]);
+        }
+
+        // delete empty black box
+        if (
+          output[output.length - 1] === "...." &&
+          output[output.length - 2] === "...."
+        ) {
+          output.pop();
+          output.pop();
+        }
+      }
+
+      // create target directory for adoc with pngs if not existant
+      if (!fs.existsSync(TARGET_PATH)) {
+        fs.mkdirSync(TARGET_PATH);
+      }
+
+      fs.writeFile(
+        `${TARGET_PATH}${file}`,
+        output.join("\n"),
+        "utf8",
+        (err) => {
+          if (err) throw err;
+          console.log(
+            `successfully replaced PlantUML Code inside ${file} with .PNG Images`
           );
         }
-        imageList.push(image);
-      }
-    );
-
-    if (!imageList.length) {
-      console.log("No Images to replace.");
-      return;
-    }
-
-    let ImageDirectoryPathForAdoc = IMAGES_PATH + path.parse(file).name + "/";
-
-    const lines = data.split("\n");
-    let insidePlantUmlBlock = false;
-    const output = [];
-
-    for (let i = 0; i < lines.length; i++) {
-      // when line starts with plantuml tag replace with corresponding .PNG
-      if (lines[i].startsWith("[plantuml, target=")) {
-        const filename = lines[i].match(/target=(.*), format=svg/)[1];
-        output.push(
-          `image::${"." + ImageDirectoryPathForAdoc + imageList.shift()}[]`
-        );
-      } else if (lines[i].startsWith("@startuml")) {
-        insidePlantUmlBlock = true;
-      } else if (lines[i].startsWith("@enduml")) {
-        insidePlantUmlBlock = false;
-        continue;
-      } else if (insidePlantUmlBlock) {
-        continue;
-      } else {
-        output.push(lines[i]);
-      }
-
-      // delete empty black box
-      if (
-        output[output.length - 1] === "...." &&
-        output[output.length - 2] === "...."
-      ) {
-        output.pop();
-        output.pop();
-      }
-    }
-
-    // create target directory for adoc with pngs if not existant
-    if (!fs.existsSync("./adocs-with-pngs")) {
-      fs.mkdirSync("./adocs-with-pngs");
-    }
-
-    fs.writeFile(
-      `./adocs-with-pngs/${file}`,
-      output.join("\n"),
-      "utf8",
-      (err) => {
-        if (err) throw err;
-        console.log(
-          `successfully replaced PlantUML Code inside ${file} with .PNG Images`
-        );
-      }
-    );
-  });
+      );
+    });
+  }
 });
