4fa77aec6050b0c196c3293ee2ce377e278965af
Jaro Hartmann <jaro.hartmann@doubleslash.de>
2023-03-03 11:38:39 +0100

    feat(load-test):[TRI-387] Add gatling load test module

---
+++ irs-load-tests/src/test/java/org/eclipse/tractusx/irs/IRSLoadTestSimulation.java
@@ -0,0 +1,59 @@
+package org.eclipse.tractusx.irs;
+
+import static io.gatling.javaapi.core.CoreDsl.RawFileBody;
+import static io.gatling.javaapi.core.CoreDsl.StringBody;
+import static io.gatling.javaapi.core.CoreDsl.atOnceUsers;
+import static io.gatling.javaapi.core.CoreDsl.jsonPath;
+import static io.gatling.javaapi.core.CoreDsl.scenario;
+import static io.gatling.javaapi.http.HttpDsl.http;
+import static io.gatling.javaapi.http.HttpDsl.status;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import io.gatling.javaapi.core.ScenarioBuilder;
+import io.gatling.javaapi.core.Simulation;
+import io.gatling.javaapi.http.HttpProtocolBuilder;
+
+public class IRSLoadTestSimulation extends Simulation {
+    {
+        final String keycloak_host = System.getenv("KEYCLOAK_HOST");
+        final String clientSecret = System.getenv("KEYCLOAK_CLIENT_SECRET");
+        final String clientId = System.getenv("KEYCLOAK_CLIENT_ID");
+        String body = "grant_type=client_credentials&client_id=" + clientId + "&client_secret=" + clientSecret;
+        final String irsUrl = System.getenv("IRS_HOST");
+
+        Map<CharSequence, String> headers_0 = new HashMap<>();
+        headers_0.put("Content-Type", "application/x-www-form-urlencoded");
+
+        HttpProtocolBuilder httpProtocol = http.baseUrl(irsUrl)
+                                               .acceptHeader("*/*");
+
+        Map<CharSequence, String> headers_1 = new HashMap<>();
+        headers_1.put("Authorization", "Bearer #{access_token}");
+        headers_1.put("Content-Type", "application/json");
+
+        ScenarioBuilder scn = scenario("IRS Load Test")
+                .exec(http("Get access token")
+                        .post(keycloak_host)
+                        .body(StringBody(body))
+                        .asFormUrlEncoded()
+                        .headers(headers_0)
+                        .check(status().is(200))
+                        .check(jsonPath(
+                                "$.access_token")
+                                .saveAs("access_token")))
+                .pause(10)
+                .exec(http("Start Job")
+                        .post(irsUrl+"/irs/jobs")
+                        .headers(headers_1)
+                        .body(RawFileBody("org/eclipse/tractusx/irs/loadtest/IRS-start-job-body.json"))
+                        .check(jsonPath("$.id")
+                                .saveAs("id")))
+                .exec(http("Get Job")
+                        .get("/irs/jobs/#{id}?returnUncompletedJob=true")
+                        .headers(headers_1));
+
+        setUp(scn.injectOpen(atOnceUsers(10))).protocols(httpProtocol);
+    }
+}
