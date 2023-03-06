71426848bcfc0ff32de2e20ce7eaf8276045ff11
Adam Bugajewski <adam.bugajewski@qualityminds.pl>
2023-03-06 11:32:26 +0100

    chore(api):{TRI-1083] Replace pandoc with downdoc for MD conversion - fix for https links

--- docs/src/post-processing/fix_https_links.js
+++ docs/src/post-processing/fix_https_links.js
@@ -12,11 +12,17 @@ fs.readdirSync(PATH_TO_MD_FILES).forEach((file) => {
 
       for (let i = 0; i < lines.length; i++) {
         // when line contains https without marks around we need to add <> before and after (MD034 - Bare URL used)
-        if (lines[i].includes(" https://")) {
-          const firstIndex = lines[i].indexOf(" https://");
+        if (lines[i].includes(" https://") || lines[i].startsWith("https://")) {
+          const firstIndex = lines[i].indexOf("https://");
           const lastIndex = lines[i].indexOf(" ", firstIndex + 1);
-          const fixFirst = lines[i].replace(" https://", " <https://");
-          const afterReplacement = fixFirst.slice(0, lastIndex + 1) + ">" + fixFirst.slice(lastIndex + 1);
+          const fixFirst = lines[i].replace("https://", "<https://");
+          let afterReplacement;
+          if (lastIndex === -1) {
+            afterReplacement = fixFirst + ">";
+          } else {
+            afterReplacement = fixFirst.slice(0, lastIndex + 1) + ">" + fixFirst.slice(lastIndex + 1);
+          }
+
           output.push(afterReplacement);
         } else {
           output.push(lines[i]);
