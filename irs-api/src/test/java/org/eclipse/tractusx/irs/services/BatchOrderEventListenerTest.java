3ae7fc69f81b86018f6c620ba81eedd0eb91f6e4
Adam Bugajewski <adam.bugajewski@qualityminds.pl>
2023-04-03 15:26:48 +0200

    feat(impl):[TRI-1216] Add additiional listener to JobProcessingFinishedEvent event - new test for BatchOrderEventListener

---
+++ irs-api/src/test/java/org/eclipse/tractusx/irs/services/BatchOrderEventListenerTest.java
@@ -0,0 +1,80 @@
+package org.eclipse.tractusx.irs.services;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.eclipse.tractusx.irs.component.JobHandle;
+import org.eclipse.tractusx.irs.component.enums.ProcessingState;
+import org.eclipse.tractusx.irs.connector.batch.Batch;
+import org.eclipse.tractusx.irs.connector.batch.BatchOrder;
+import org.eclipse.tractusx.irs.connector.batch.BatchOrderStore;
+import org.eclipse.tractusx.irs.connector.batch.BatchStore;
+import org.eclipse.tractusx.irs.connector.batch.InMemoryBatchOrderStore;
+import org.eclipse.tractusx.irs.connector.batch.InMemoryBatchStore;
+import org.eclipse.tractusx.irs.connector.batch.JobProgress;
+import org.eclipse.tractusx.irs.services.events.BatchOrderRegisteredEvent;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationEventPublisher;
+
+class BatchOrderEventListenerTest {
+
+    private final UUID BATCH_ORDER_ID = UUID.randomUUID();
+    private final UUID FIRST_BATCH_ID = UUID.randomUUID();
+    private final UUID SECOND_BATCH_ID = UUID.randomUUID();
+    private BatchOrderStore batchOrderStore;
+    private BatchStore batchStore;
+    private final IrsItemGraphQueryService irsItemGraphQueryService = mock(IrsItemGraphQueryService.class);
+    private final ApplicationEventPublisher applicationEventPublisher = mock(ApplicationEventPublisher.class);
+
+    private BatchOrderEventListener eventListener;
+
+    @BeforeEach
+    void beforeEach() {
+        batchOrderStore = new InMemoryBatchOrderStore();
+        batchStore = new InMemoryBatchStore();
+        eventListener = new BatchOrderEventListener(batchOrderStore, batchStore, irsItemGraphQueryService, applicationEventPublisher);
+    }
+
+    @Test
+    void shouldStartFirstBatch() {
+        // given
+        final int numberOfJobs = 10;
+        final BatchOrder batchOrder = BatchOrder.builder()
+                                                .batchOrderId(BATCH_ORDER_ID)
+                                                .batchOrderState(ProcessingState.INITIALIZED)
+                .collectAspects(Boolean.TRUE)
+                                                .build();
+        final Batch firstBatch = Batch.builder()
+                                      .batchId(FIRST_BATCH_ID)
+                                      .batchState(ProcessingState.PARTIAL).batchNumber(1).batchOrderId(BATCH_ORDER_ID)
+                                      .jobProgressList(createJobProgressList(numberOfJobs)).build();
+        final Batch secondBatch = Batch.builder().batchId(SECOND_BATCH_ID)
+                                       .batchState(ProcessingState.PARTIAL).batchNumber(2).batchOrderId(BATCH_ORDER_ID).build();
+
+        given(irsItemGraphQueryService.registerItemJob(any(), any())).willReturn(JobHandle.builder().id(UUID.randomUUID()).build());
+
+        batchOrderStore.save(BATCH_ORDER_ID, batchOrder);
+        batchStore.save(FIRST_BATCH_ID, firstBatch);
+        batchStore.save(SECOND_BATCH_ID, secondBatch);
+        // when
+        eventListener.handleBatchOrderRegisteredEvent(new BatchOrderRegisteredEvent(BATCH_ORDER_ID));
+        // then
+        verify(irsItemGraphQueryService, times(numberOfJobs)).registerItemJob(any(), eq(FIRST_BATCH_ID));
+    }
+
+    private List<JobProgress> createJobProgressList(Integer size) {
+        return IntStream.range(0, size).boxed().map(i -> JobProgress.builder().globalAssetId(i.toString()).build()).collect(
+                Collectors.toList());
+    }
+
+}
