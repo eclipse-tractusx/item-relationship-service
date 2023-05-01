c8d627cdc2ba118d35738cc45ac2f448b1dc1911
Krzysztof Massalski (Extern) <Krzysztof.Massalski@partner.doubleslash.de>
2023-05-01 12:42:39 +0200

    feat(impl):[TRI-1080] fix test

--- irs-api/src/test/java/org/eclipse/tractusx/irs/controllers/BatchControllerTest.java
+++ irs-api/src/test/java/org/eclipse/tractusx/irs/controllers/BatchControllerTest.java
@@ -43,6 +43,7 @@ import org.eclipse.tractusx.irs.services.QueryBatchService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
@@ -52,6 +53,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(value = BatchController.class, excludeAutoConfiguration = { SecurityAutoConfiguration.class})
 @Import(SecurityConfiguration.class)
+@AutoConfigureMockMvc(addFilters = true)
 class BatchControllerTest {
 
     @Autowired
