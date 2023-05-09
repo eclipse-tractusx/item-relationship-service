03e0786c1aa5f6c64a412b5daf5f62a59ed58fde
Jaro Hartmann <jaro.hartmann@doubleslash.de>
2023-05-09 13:44:01 +0200

    feat(local): Add discovery service mocks to irs-full

--- local/deployment/full-irs/forwardingPorts.sh
+++ local/deployment/full-irs/forwardingPorts.sh
@@ -9,6 +9,7 @@ kubectl port-forward svc/edc-daps 4567:4567 -n irs &
 kubectl port-forward svc/edc-provider-control-plane 6080:8080 6181:8181 -n irs &
 kubectl port-forward svc/edc-consumer-control-plane 7080:8080 7181:8181 -n irs &
 kubectl port-forward svc/semantic-hub 8088:8080 -n irs &
+kubectl port-forward svc/discovery 8888:8080 -n irs &
 kubectl port-forward svc/cx-irs-dependencies-registry-svc 10200:8080 -n irs &
 kubectl port-forward svc/keycloak 4011:8080 -n irs &
 kubectl port-forward svc/irs-provider-backend 10199:8080 -n irs &
