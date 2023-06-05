9b089871045436d7f4abf7e667024cc2c9280fe5
Adam Bugajewski <adam.bugajewski@qualityminds.pl>
2023-06-05 18:34:52 +0200

    feat(impl):[TRI-1349] Decentral DT registry aas requesting flow - using Endpoint References in DT registry client

--- irs-api/src/main/java/org/eclipse/tractusx/irs/aaswrapper/registry/domain/DecentralDigitalTwinRegistryClient.java
+++ irs-api/src/main/java/org/eclipse/tractusx/irs/aaswrapper/registry/domain/DecentralDigitalTwinRegistryClient.java
@@ -22,17 +22,21 @@
  ********************************************************************************/
 package org.eclipse.tractusx.irs.aaswrapper.registry.domain;
 
-import static org.eclipse.tractusx.irs.configuration.RestTemplateConfig.DTR_REST_TEMPLATE;
-
 import java.util.List;
 import java.util.Map;
 
 import io.github.resilience4j.retry.annotation.Retry;
+import jakarta.validation.constraints.Max;
+import lombok.RequiredArgsConstructor;
 import org.eclipse.dataspaceconnector.spi.types.domain.edr.EndpointDataReference;
 import org.eclipse.tractusx.irs.component.assetadministrationshell.AssetAdministrationShellDescriptor;
 import org.eclipse.tractusx.irs.component.assetadministrationshell.IdentifierKeyValuePair;
 import org.eclipse.tractusx.irs.util.JsonUtil;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -41,32 +45,43 @@ import org.springframework.web.util.UriComponentsBuilder;
  * Digital Twin Registry Rest Client
  */
 @Service
+@RequiredArgsConstructor
 public class DecentralDigitalTwinRegistryClient {
 
     private static final String PLACEHOLDER_AAS_IDENTIFIER = "aasIdentifier";
     private static final String PLACEHOLDER_ASSET_IDS = "assetIds";
     private static final String SHELL_DESCRIPTOR_TEMPLATE = "/registry/shell-descriptors/{aasIdentifier}";
     private static final String LOOKUP_SHELLS_TEMPLATE = "/lookup/shells?assetIds={assetIds}";
-
-    private final RestTemplate restTemplate;
-
-    public DecentralDigitalTwinRegistryClient(@Qualifier(DTR_REST_TEMPLATE) final RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
+    private final RestTemplate edcRestTemplate;
 
     @Retry(name = "registry")
-    public AssetAdministrationShellDescriptor getAssetAdministrationShellDescriptor(final EndpointDataReference endpointDataReference, final String aasIdentifier) {
+    public AssetAdministrationShellDescriptor getAssetAdministrationShellDescriptor(
+            final EndpointDataReference endpointDataReference, final String aasIdentifier) {
         final String descriptorEndpoint = endpointDataReference.getEndpoint() + SHELL_DESCRIPTOR_TEMPLATE;
         final UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromUriString(descriptorEndpoint);
         final Map<String, String> values = Map.of(PLACEHOLDER_AAS_IDENTIFIER, aasIdentifier);
-        return restTemplate.getForObject(uriBuilder.build(values), AssetAdministrationShellDescriptor.class);
+        return edcRestTemplate.exchange(uriBuilder.build(values), HttpMethod.GET,
+                                      new HttpEntity<>(null, headers(endpointDataReference)), AssetAdministrationShellDescriptor.class)
+                              .getBody();
     }
 
     @Retry(name = "registry")
-    public List<String> getAllAssetAdministrationShellIdsByAssetLink(final EndpointDataReference endpointDataReference, final List<IdentifierKeyValuePair> assetIds) {
+    public List<String> getAllAssetAdministrationShellIdsByAssetLink(final EndpointDataReference endpointDataReference,
+            final List<IdentifierKeyValuePair> assetIds) {
         final String shellLookupEndpoint = endpointDataReference.getEndpoint() + LOOKUP_SHELLS_TEMPLATE;
         final UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromUriString(shellLookupEndpoint);
         final var values = Map.of(PLACEHOLDER_ASSET_IDS, new JsonUtil().asString(assetIds));
-        return restTemplate.getForObject(uriBuilder.build(values), List.class);
+        return edcRestTemplate.exchange(uriBuilder.build(values), HttpMethod.GET,
+                new HttpEntity<>(null, headers(endpointDataReference)), List.class).getBody();
+    }
+
+    private HttpHeaders headers(final EndpointDataReference dataReference) {
+        final HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+        final var authKey = dataReference.getAuthKey();
+        if (authKey != null) {
+            headers.add(authKey, dataReference.getAuthCode());
+        }
+        return headers;
     }
 }
--- irs-api/src/test/java/org/eclipse/tractusx/irs/aaswrapper/registry/domain/DecentralDigitalTwinRegistryClientTest.java
+++ irs-api/src/test/java/org/eclipse/tractusx/irs/aaswrapper/registry/domain/DecentralDigitalTwinRegistryClientTest.java
@@ -1,14 +1,19 @@
 package org.eclipse.tractusx.irs.aaswrapper.registry.domain;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import org.eclipse.dataspaceconnector.spi.types.domain.edr.EndpointDataReference;
 import org.eclipse.tractusx.irs.component.assetadministrationshell.AssetAdministrationShellDescriptor;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 class DecentralDigitalTwinRegistryClientTest {
@@ -19,28 +24,35 @@ class DecentralDigitalTwinRegistryClientTest {
     void shouldCallForAssetAdministrationShellDescriptor() {
         // given
         DecentralDigitalTwinRegistryClient client = new DecentralDigitalTwinRegistryClient(restTemplate);
-        EndpointDataReference endpointDataReference = EndpointDataReference.Builder.newInstance().endpoint("url.to.host").build();
-        Mockito.when(restTemplate.getForObject(any(), any())).thenReturn(AssetAdministrationShellDescriptor.builder().build());
+        EndpointDataReference endpointDataReference = EndpointDataReference.Builder.newInstance()
+                                                                                   .endpoint("url.to.host")
+                                                                                   .build();
+        Mockito.when(
+                       restTemplate.exchange(any(), eq(HttpMethod.GET), any(), eq(AssetAdministrationShellDescriptor.class)))
+               .thenReturn(ResponseEntity.of(Optional.of(AssetAdministrationShellDescriptor.builder().build())));
 
         // when
         client.getAssetAdministrationShellDescriptor(endpointDataReference, "aas-id");
 
         // then
-        verify(restTemplate).getForObject(any(), any());
+        verify(restTemplate).exchange(any(), eq(HttpMethod.GET), any(), eq(AssetAdministrationShellDescriptor.class));
     }
 
     @Test
     void shouldCallForAllAssetAdministrationShellIdsByAssetLink() {
         // given
         DecentralDigitalTwinRegistryClient client = new DecentralDigitalTwinRegistryClient(restTemplate);
-        EndpointDataReference endpointDataReference = EndpointDataReference.Builder.newInstance().endpoint("url.to.host").build();
-        Mockito.when(restTemplate.getForObject(any(), any())).thenReturn(new ArrayList<String>());
+        EndpointDataReference endpointDataReference = EndpointDataReference.Builder.newInstance()
+                                                                                   .endpoint("url.to.host")
+                                                                                   .build();
+        Mockito.when(restTemplate.exchange(any(), eq(HttpMethod.GET), any(), eq(List.class)))
+               .thenReturn(ResponseEntity.of(Optional.of(new ArrayList<>())));
 
         // when
         client.getAllAssetAdministrationShellIdsByAssetLink(endpointDataReference, new ArrayList<>());
 
         // then
-        verify(restTemplate).getForObject(any(), any());
+        verify(restTemplate).exchange(any(), eq(HttpMethod.GET), any(), eq(List.class));
     }
 
 }
