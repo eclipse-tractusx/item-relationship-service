afd52aaf08417378faeb9b57af2d56e4a0ba7579
ds-ext-abugajewski <112549278+ds-ext-abugajewski@users.noreply.github.com>
2023-06-16 08:22:51 +0200

    feat(impl):[TRI-1307] [PRIO 3] Migration to AAS V3.0 of Asset Administration Shell Specification of Digital Twin APIs (Digital Twin Registry and GetSubmodel) - unused imports

--- irs-models/src/main/java/org/eclipse/tractusx/irs/component/assetadministrationshell/AssetAdministrationShellDescriptor.java
+++ irs-models/src/main/java/org/eclipse/tractusx/irs/component/assetadministrationshell/AssetAdministrationShellDescriptor.java
@@ -30,9 +30,6 @@ import java.util.stream.Collectors;
 
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
