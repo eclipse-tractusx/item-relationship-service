b562ec4536d9ea1b582852cd730c08bbdd67cba9
Krzysztof Massalski (Extern) <Krzysztof.Massalski@partner.doubleslash.de>
2023-08-07 14:10:42 +0200

    feat(impl):[TRI-1093] data integrity delegate test

--- irs-api/src/test/java/org/eclipse/tractusx/irs/aaswrapper/job/delegate/IntegrityDelegateTest.java
+++ irs-api/src/test/java/org/eclipse/tractusx/irs/aaswrapper/job/delegate/IntegrityDelegateTest.java
@@ -41,13 +41,14 @@ import java.util.Objects;
 import org.eclipse.tractusx.irs.aaswrapper.job.AASTransferProcess;
 import org.eclipse.tractusx.irs.aaswrapper.job.ItemContainer;
 import org.eclipse.tractusx.irs.component.PartChainIdentificationKey;
+import org.eclipse.tractusx.irs.component.enums.ProcessStep;
 import org.eclipse.tractusx.irs.edc.client.EdcSubmodelFacade;
 import org.eclipse.tractusx.irs.edc.client.exceptions.EdcClientException;
 import org.eclipse.tractusx.irs.registryclient.discovery.ConnectorEndpointsService;
 import org.eclipse.tractusx.irs.util.JsonUtil;
 import org.junit.jupiter.api.Test;
 
-public class IntegrityDelegateTest {
+class IntegrityDelegateTest {
 
     final EdcSubmodelFacade submodelFacade = mock(EdcSubmodelFacade.class);
     final ConnectorEndpointsService connectorEndpointsService = mock(ConnectorEndpointsService.class);
@@ -81,5 +82,49 @@ public class IntegrityDelegateTest {
         assertThat(result).isNotNull();
         assertThat(result.getIntegrities()).isNotEmpty();
         assertThat(result.getIntegrities().stream().findAny().get().getChildParts()).isNotEmpty();
+    }
+
+    @Test
+    void shouldCatchRestClientExceptionAndPutTombstone() throws EdcClientException {
+        // given
+        when(submodelFacade.getSubmodelRawPayload(anyString(), anyString(), anyString())).thenThrow(
+                new EdcClientException("Unable to call endpoint"));
+        when(connectorEndpointsService.fetchConnectorEndpoints(any())).thenReturn(List.of("http://localhost"));
+
+        final ItemContainer.ItemContainerBuilder itemContainerWithShell = ItemContainer.builder()
+                                                                                       .shell(shellDescriptor(
+                                                                                               List.of(submodelDescriptor(
+                                                                                                       "DataIntegrity",
+                                                                                                       "address"))));
+
+        // when
+        final ItemContainer result = integrityDelegate.process(itemContainerWithShell, jobParameter(),
+                new AASTransferProcess(), createKey());
+
+        // then
+        assertThat(result).isNotNull();
+        assertThat(result.getTombstones()).hasSize(1);
+        assertThat(result.getTombstones().get(0).getCatenaXId()).isEqualTo("itemId");
+        assertThat(result.getTombstones().get(0).getProcessingError().getProcessStep()).isEqualTo(
+                ProcessStep.DATA_INTEGRITY_CHECK);
+    }
+
+    @Test
+    void shouldPutTombstoneForMissingBpn() {
+        final ItemContainer.ItemContainerBuilder itemContainerWithShell = ItemContainer.builder()
+                                                                                       .shell(shellDescriptor(
+                                                                                               List.of(submodelDescriptor(
+                                                                                                       "DataIntegrity",
+                                                                                                       "address"))));
+        // when
+        final ItemContainer result = integrityDelegate.process(itemContainerWithShell, jobParameter(),
+                new AASTransferProcess(), PartChainIdentificationKey.builder().globalAssetId("testId").build());
+
+        // then
+        assertThat(result).isNotNull();
+        assertThat(result.getTombstones()).hasSize(1);
+        assertThat(result.getTombstones().get(0).getCatenaXId()).isEqualTo("testId");
+        assertThat(result.getTombstones().get(0).getProcessingError().getProcessStep()).isEqualTo(
+                ProcessStep.DATA_INTEGRITY_CHECK);
     }
 }
