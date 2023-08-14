1ce50c02c60cfe27a629a4ff9556588674c155cc
Krzysztof Massalski (Extern) <Krzysztof.Massalski@partner.doubleslash.de>
2023-08-14 16:00:15 +0200

    feat(impl):[TRI-1093] additional unit test

--- irs-api/src/test/java/org/eclipse/tractusx/irs/services/DataIntegrityServiceTest.java
+++ irs-api/src/test/java/org/eclipse/tractusx/irs/services/DataIntegrityServiceTest.java
@@ -55,20 +55,13 @@ class DataIntegrityServiceTest {
     }
 
     @Test
-    void shouldValidateDataChainIntegrityWhenHashesMatch() {
+    void shouldValidateDataChainIntegrityWhenHashAndSignatureMatches() {
         // Arrange
         final IntegrityAspect.Reference reference = new IntegrityAspect.Reference("testType",
-                "3d9a14153459ef617d86116297fb63d37948bffc9247bb969b63af29fe9aac6f", "65998c8046dec62a8863c83ca150fa81e30ea44a88d50e4368d6bdd3e12ee59535e862085527204d684292747761cbc57cdd258a052d4509b6edfb952c2127f3935da1dd57ea9b843e111bc3825d367f5d52b6491f91456007936bb635a38eb24cc19cff723a96a08f6fff829ccae633b3cec55e48ab870a211ee87b77ce6ea8");
+                "3d9a14153459ef617d86116297fb63d37948bffc9247bb969b63af29fe9aac6f", "a330b06d73ec0747fd7da4c099f5687eba57766f8a33ae02ba23c2f239a82a6c14b9701c1d57f946de4363453a4974231a02180fc2f44be313b2da3a83cd56ef4077ac75ce2bab32d7a6402dfa7ab98a4e54320b66b9d087d07ee2f516fab929d270485d666b96872ae251881e3698506905b28842b070e1e8ffb8509f0a183396754fdadc75028b72b3e23aa7525d4b47cf4af947b9c2715ea46cb61cc5a834e35496b03a624da92b65ebe2124d76e5cf0e5520fc0841d33b80072a05e595c5c29d9db6b5e94ec3c83efd11b80ff639e168dd578b6b92d4e8143dfdcedf43495654f5ccd388f0233c082652a1c1eb8bfccae6d065b481f653674773a4cbbb07");
         final IntegrityAspect.ChildData childData = new IntegrityAspect.ChildData("child-id", Set.of(reference));
         final IntegrityAspect integrityAspect = new IntegrityAspect("parent-id", Set.of(childData));
-        final String payload = "{\"test\":\"test\"}";
-        final Submodel submodel = Submodel.builder()
-                                          .aspectType("testType")
-                                          .catenaXId("child-id")
-                                          .identification("child-identifier")
-                                          .payload(mapFromString(payload, Map.class))
-                                          .build();
-        final ItemContainer build = ItemContainer.builder().submodel(submodel).integrity(integrityAspect).build();
+        final ItemContainer build = ItemContainer.builder().submodel(getSubmodel()).integrity(integrityAspect).build();
 
         // Act
         final IntegrityState integrityState = dataIntegrityService.chainDataIntegrityIsValid(build);
@@ -83,19 +76,38 @@ class DataIntegrityServiceTest {
         final IntegrityAspect.Reference reference = new IntegrityAspect.Reference("testType", "wrongHash", null);
         final IntegrityAspect.ChildData childData = new IntegrityAspect.ChildData("child-id", Set.of(reference));
         final IntegrityAspect integrityAspect = new IntegrityAspect("parent-id", Set.of(childData));
-        final String payload = "{\"test\":\"test\"}";
-        final Submodel submodel = Submodel.builder()
-                                          .aspectType("testType")
-                                          .catenaXId("child-id")
-                                          .identification("child-identifier")
-                                          .payload(mapFromString(payload, Map.class))
-                                          .build();
-        final ItemContainer build = ItemContainer.builder().submodel(submodel).integrity(integrityAspect).build();
+        final ItemContainer build = ItemContainer.builder().submodel(getSubmodel()).integrity(integrityAspect).build();
 
         // Act
         final IntegrityState integrityState = dataIntegrityService.chainDataIntegrityIsValid(build);
 
         // Assert
         assertThat(integrityState).isEqualTo(IntegrityState.INVALID);
+    }
+
+    @Test
+    void shouldFailDataChainValidationWhenPresentedSignatureIsWrong() {
+        // Arrange
+        final IntegrityAspect.Reference reference = new IntegrityAspect.Reference("testType", "3d9a14153459ef617d86116297fb63d37948bffc9247bb969b63af29fe9aac6f", "30b06d73ec07");
+        final IntegrityAspect.ChildData childData = new IntegrityAspect.ChildData("child-id", Set.of(reference));
+        final IntegrityAspect integrityAspect = new IntegrityAspect("parent-id", Set.of(childData));
+        final ItemContainer build = ItemContainer.builder().submodel(getSubmodel()).integrity(integrityAspect).build();
+
+        // Act
+        final IntegrityState integrityState = dataIntegrityService.chainDataIntegrityIsValid(build);
+
+        // Assert
+        assertThat(integrityState).isEqualTo(IntegrityState.INVALID);
+    }
+
+    private Submodel getSubmodel() {
+        final String payload = "{\"test\":\"test\"}";
+
+        return Submodel.builder()
+                       .aspectType("testType")
+                       .catenaXId("child-id")
+                       .identification("child-identifier")
+                       .payload(mapFromString(payload, Map.class))
+                       .build();
     }
 }
