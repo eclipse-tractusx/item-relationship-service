555544d460fff6fda8e5f8ac9c42afad7d583ebc
ds-ext-abugajewski <112549278+ds-ext-abugajewski@users.noreply.github.com>
2023-09-15 16:55:41 +0200

    feat(irs-policy-store): [TRI-1593] Policy Store API Extension - fix checkstyle and merge main

--- irs-policy-store/src/main/java/org/eclipse/tractusx/irs/policystore/models/OperatorType.java
+++ irs-policy-store/src/main/java/org/eclipse/tractusx/irs/policystore/models/OperatorType.java
@@ -11,7 +11,8 @@
  *
  * This program and the accompanying materials are made available under the
  * terms of the Apache License, Version 2.0 which is available at
- * https://www.apache.org/licenses/LICENSE-2.0. *
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -37,19 +38,19 @@ import lombok.Getter;
 @Getter
 public enum OperatorType {
 
-    EQ ("eq", "Equals to"),
-    NEQ ("neq", "Not equal to"),
-    LT ("lt", "Less than"),
-    GT ("gt", "Greater than"),
-    IN ("in", "In"),
-    LTEQ ("lteq", "Less than or equal to"),
-    GTEQ ("gteq", "Greater than or equal to"),
-    ISA ("isA", "Is a"),
-    HASPART ("hasPart", "Has part"),
-    ISPARTOF ("isPartOf", "Is part of"),
-    ISONEOF ("isOneOf", "Is one of"),
-    ISALLOF ("isAllOf", "Is all of"),
-    ISNONEOF ("isNoneOf", "Is none of");
+    EQ("eq", "Equals to"),
+    NEQ("neq", "Not equal to"),
+    LT("lt", "Less than"),
+    GT("gt", "Greater than"),
+    IN("in", "In"),
+    LTEQ("lteq", "Less than or equal to"),
+    GTEQ("gteq", "Greater than or equal to"),
+    ISA("isA", "Is a"),
+    HASPART("hasPart", "Has part"),
+    ISPARTOF("isPartOf", "Is part of"),
+    ISONEOF("isOneOf", "Is one of"),
+    ISALLOF("isAllOf", "Is all of"),
+    ISNONEOF("isNoneOf", "Is none of");
 
     private final String code;
     private final String label;
