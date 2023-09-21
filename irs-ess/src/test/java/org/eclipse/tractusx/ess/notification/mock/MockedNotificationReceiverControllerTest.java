21078910ebd02e59fec35e446c7c8109bff124b8
Jaro Hartmann <jaro.hartmann@doubleslash.de>
2023-09-21 09:33:14 +0200

    feat(ess):[TRI-1523] Remove unused fields and imports

--- irs-ess/src/test/java/org/eclipse/tractusx/ess/notification/mock/MockedNotificationReceiverControllerTest.java
+++ irs-ess/src/test/java/org/eclipse/tractusx/ess/notification/mock/MockedNotificationReceiverControllerTest.java
@@ -23,7 +23,6 @@
  ********************************************************************************/
 package org.eclipse.tractusx.ess.notification.mock;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -33,18 +32,14 @@ import static org.mockito.Mockito.when;
 import java.util.Map;
 import java.util.UUID;
 
+import org.eclipse.tractusx.ess.discovery.EdcDiscoveryMockConfig;
+import org.eclipse.tractusx.ess.service.SupplyChainImpacted;
 import org.eclipse.tractusx.irs.common.auth.IrsRoles;
 import org.eclipse.tractusx.irs.edc.client.EdcSubmodelFacade;
 import org.eclipse.tractusx.irs.edc.client.model.notification.EdcNotification;
 import org.eclipse.tractusx.irs.edc.client.model.notification.EdcNotificationHeader;
-import org.eclipse.tractusx.ess.discovery.EdcDiscoveryMockConfig;
-import org.eclipse.tractusx.ess.service.EssService;
-import org.eclipse.tractusx.ess.service.SupplyChainImpacted;
-import org.eclipse.tractusx.irs.component.RegisterBpnInvestigationJob;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -66,58 +61,57 @@ class MockedNotificationReceiverControllerTest {
     @Mock
     private EdcDiscoveryMockConfig edcDiscoveryMockConfig;
 
-    @Mock
-    private EssService essService;
-
-    @Captor
-    private ArgumentCaptor<RegisterBpnInvestigationJob> jobArgumentCaptor;
-
     @Test
     @WithMockUser(authorities = IrsRoles.VIEW_IRS)
     void shouldReceiveNotificationAndSendMockedNotificationResult() throws Exception {
         final String bpn = "BPN1";
         when(edcDiscoveryMockConfig.getMockEdcResult()).thenReturn(Map.of(bpn, SupplyChainImpacted.YES));
-        when(edcSubmodelFacade.sendNotification(anyString(), anyString(), any(EdcNotification.class))).thenReturn(() -> true);
+        when(edcSubmodelFacade.sendNotification(anyString(), anyString(), any(EdcNotification.class))).thenReturn(
+                () -> true);
         RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(new MockHttpServletRequest()));
 
-        testee.receiveNotification(EdcNotification.builder().header(validHeader()).content(Map.of("incidentBpn", bpn)).build());
+        testee.receiveNotification(
+                EdcNotification.builder().header(validHeader()).content(Map.of("incidentBpn", bpn)).build());
 
         verify(edcSubmodelFacade).sendNotification(anyString(), anyString(), any(EdcNotification.class));
     }
 
     @Test
     void shouldReturnBadRequestIfNotificationHeaderBodyNotValid() {
-        final EdcNotification request = EdcNotification.builder().header(EdcNotificationHeader.builder().build()).content(Map.of()).build();
+        final EdcNotification request = EdcNotification.builder()
+                                                       .header(EdcNotificationHeader.builder().build())
+                                                       .content(Map.of())
+                                                       .build();
 
-        assertThrows(ResponseStatusException.class,
-                () -> testee.receiveNotification(request));
+        assertThrows(ResponseStatusException.class, () -> testee.receiveNotification(request));
     }
 
     @Test
     void shouldReturnBadRequestIfIncidentBpnNotInRequestBody() {
         final EdcNotification request = EdcNotification.builder().header(validHeader()).content(Map.of()).build();
 
-        assertThrows(ResponseStatusException.class,
-                () -> testee.receiveNotification(request));
+        assertThrows(ResponseStatusException.class, () -> testee.receiveNotification(request));
     }
 
     @Test
     void shouldReturnBadRequestIfIncidentBpnNotInMockedMapResult() {
-        final EdcNotification request = EdcNotification.builder().header(validHeader()).content(Map.of("incidentBpn", "BPN")).build();
+        final EdcNotification request = EdcNotification.builder()
+                                                       .header(validHeader())
+                                                       .content(Map.of("incidentBpn", "BPN"))
+                                                       .build();
 
-        assertThrows(ResponseStatusException.class,
-                () -> testee.receiveNotification(request));
+        assertThrows(ResponseStatusException.class, () -> testee.receiveNotification(request));
     }
 
     EdcNotificationHeader validHeader() {
         return EdcNotificationHeader.builder()
-                                      .notificationId(UUID.randomUUID().toString())
-                                      .senderEdc("senderEdc")
-                                      .senderBpn("senderBpn")
-                                      .recipientBpn("recipientBpn")
-                                      .replyAssetId("ess-response-asset")
-                                      .replyAssetSubPath("")
-                                      .notificationType("ess-supplier-request")
-                                      .build();
+                                    .notificationId(UUID.randomUUID().toString())
+                                    .senderEdc("senderEdc")
+                                    .senderBpn("senderBpn")
+                                    .recipientBpn("recipientBpn")
+                                    .replyAssetId("ess-response-asset")
+                                    .replyAssetSubPath("")
+                                    .notificationType("ess-supplier-request")
+                                    .build();
     }
 }
