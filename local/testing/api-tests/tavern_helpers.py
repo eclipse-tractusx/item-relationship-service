d6da7f81d741f7024dfb6575fcfef28ad0c312cb
ds-alexander-bulgakov <alexander.bulgakov@partner.doubleslash.de>
2023-11-08 09:00:00 +0100

    refacroting: refactored tavern-helpers for supplyChainImpacted checks

--- local/testing/api-tests/tavern_helpers.py
+++ local/testing/api-tests/tavern_helpers.py
@@ -6,13 +6,13 @@ import os
 from box import Box
 
 
-def supplyChainImpacted_is_Yes(response):
+def supplyChainImpacted_is_as_expected(response, expectedSupplyChainImpacted):
     submodels = response.json().get("submodels")
     print("submodels ", submodels)
     assert len(submodels) <= 1
     for i in submodels:
         assert 'supply_chain_impacted' in i.get('aspectType')
-        assert 'Yes' in i.get("payload").get('supplyChainImpacted')
+        assert expectedSupplyChainImpacted in i.get("payload").get('supplyChainImpacted')
 
 
 def supplyChainFirstLevelBpn_is_as_expected(response, expectedBpnl):
@@ -29,24 +29,6 @@ def supplyChainhops_is_as_expected(response, expectedHops):
         impactedSuppliersOnFirstTier = i.get("payload").get("impactedSuppliersOnFirstTier")
         for ii in impactedSuppliersOnFirstTier:
             assert ii.get('hops') == expectedHops
-
-
-def supplyChainImpacted_is_No(response):
-    submodels = response.json().get("submodels")
-    print("submodels ", submodels)
-    assert len(submodels) <= 1
-    for i in submodels:
-        assert 'supply_chain_impacted' in i.get('aspectType')
-        assert 'No' in i.get("payload").get('supplyChainImpacted')
-
-
-def supplyChainImpacted_is_Unknown(response):
-    submodels = response.json().get("submodels")
-    print("submodels ", submodels)
-    assert len(submodels) <= 1
-    for i in submodels:
-        assert 'supply_chain_impacted' in i.get('aspectType')
-        assert 'Unknown' in i.get("payload").get('supplyChainImpacted')
 
 
 def errors_for_invalid_investigation_request_are_correct(response):
