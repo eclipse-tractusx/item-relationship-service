06b1738ad00c6070b9b3082f996e6cd12ea8b412
Matthias Fischer <matthias.fischer@doubleslash.de>
2024-02-16 12:49:14 +0100

    feat(impl):[#395] Remove obsolete todo
    this code place is not related to this story

--- irs-api/src/main/java/org/eclipse/tractusx/irs/aaswrapper/job/delegate/RelationshipDelegate.java
+++ irs-api/src/main/java/org/eclipse/tractusx/irs/aaswrapper/job/delegate/RelationshipDelegate.java
@@ -76,10 +76,9 @@ public class RelationshipDelegate extends AbstractDelegate {
         itemContainerBuilder.build()
                             .getShells()
                             .stream()
-                            // TODO (mfischer): #395 is this part of the story too? need to change?
-                            //       if yes, which step in the diagrams is this?
                             .findFirst()
-                            .ifPresent(shell -> shell.payload().findRelationshipEndpointAddresses(
+                            .ifPresent(shell -> shell.payload()
+                                                     .findRelationshipEndpointAddresses(
                                                              AspectType.fromValue(relationshipAspect.getName()))
                                                      .forEach(endpoint -> processEndpoint(endpoint, relationshipAspect,
                                                              aasTransferProcess, itemContainerBuilder, itemId)));
@@ -101,8 +100,8 @@ public class RelationshipDelegate extends AbstractDelegate {
         }
 
         try {
-            final String submodelRawPayload = requestSubmodel(submodelFacade, connectorEndpointsService,
-                    endpoint, itemId.getBpn()).getPayload();
+            final String submodelRawPayload = requestSubmodel(submodelFacade, connectorEndpointsService, endpoint,
+                    itemId.getBpn()).getPayload();
 
             final var relationships = jsonUtil.fromString(submodelRawPayload, relationshipAspect.getSubmodelClazz())
                                               .asRelationships();
@@ -118,24 +117,28 @@ public class RelationshipDelegate extends AbstractDelegate {
         } catch (final UsagePolicyException e) {
             log.info("Encountered usage policy exception: {}. Creating Tombstone.", e.getMessage());
             itemContainerBuilder.tombstone(
-                    Tombstone.from(itemId.getGlobalAssetId(), endpoint.getProtocolInformation().getHref(), e,
-                            0, ProcessStep.USAGE_POLICY_VALIDATION, jsonUtil.asMap(e.getPolicy())));
+                    Tombstone.from(itemId.getGlobalAssetId(), endpoint.getProtocolInformation().getHref(), e, 0,
+                            ProcessStep.USAGE_POLICY_VALIDATION, jsonUtil.asMap(e.getPolicy())));
         } catch (final EdcClientException e) {
             log.info("Submodel Endpoint could not be retrieved for Endpoint: {}. Creating Tombstone.",
                     endpoint.getProtocolInformation().getHref());
             itemContainerBuilder.tombstone(
-                    Tombstone.from(itemId.getGlobalAssetId(), endpoint.getProtocolInformation().getHref(), e,
-                            0, ProcessStep.SUBMODEL_REQUEST));
+                    Tombstone.from(itemId.getGlobalAssetId(), endpoint.getProtocolInformation().getHref(), e, 0,
+                            ProcessStep.SUBMODEL_REQUEST));
         } catch (final JsonParseException e) {
             log.info("Submodel payload did not match the expected AspectType. Creating Tombstone.");
             itemContainerBuilder.tombstone(
-                    Tombstone.from(itemId.getGlobalAssetId(), endpoint.getProtocolInformation().getHref(), e,
-                            0, ProcessStep.SUBMODEL_REQUEST));
+                    Tombstone.from(itemId.getGlobalAssetId(), endpoint.getProtocolInformation().getHref(), e, 0,
+                            ProcessStep.SUBMODEL_REQUEST));
         }
     }
 
     private static List<Bpn> getBpnsFrom(final List<Relationship> relationships) {
-        return relationships.stream().map(Relationship::getBpn).filter(StringUtils::isNotBlank).map(Bpn::withManufacturerId).toList();
+        return relationships.stream()
+                            .map(Relationship::getBpn)
+                            .filter(StringUtils::isNotBlank)
+                            .map(Bpn::withManufacturerId)
+                            .toList();
     }
 
     private List<PartChainIdentificationKey> getIdsToProcess(final List<Relationship> relationships,
