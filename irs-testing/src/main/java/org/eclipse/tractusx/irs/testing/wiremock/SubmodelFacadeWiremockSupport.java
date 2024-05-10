193dd267eb08f619c3cd9e407ba3b08ffeef713a
jhartmann <jaro.hartmann@doubleslash.de>
2024-05-10 16:26:11 +0200

    Revert "chore(irs-api):[#596] Add default policy wiremock tests"
    This reverts commit 67d5f4306c826923435f7015869b9ef2099bfc70.

--- irs-testing/src/main/java/org/eclipse/tractusx/irs/testing/wiremock/SubmodelFacadeWiremockSupport.java
+++ irs-testing/src/main/java/org/eclipse/tractusx/irs/testing/wiremock/SubmodelFacadeWiremockSupport.java
@@ -52,10 +52,6 @@ public final class SubmodelFacadeWiremockSupport {
     public static final String IRS_INTERNAL_CALLBACK_URL = "https://irs.test/internal/endpoint-data-reference";
     public static final String EDC_PROVIDER_BPN = "BPNL00000003CRHK";
     public static final int STATUS_CODE_OK = 200;
-    public static final String CX_POLICY_FRAMEWORK_AGREEMENT = "cx-policy:FrameworkAgreement";
-    public static final String TRACEABILITY_1_0 = "traceability:1.0";
-    public static final String CX_POLICY_USAGE_PURPOSE = "cx-policy:UsagePurpose";
-    public static final String CX_CORE_INDUSTRYCORE_1 = "cx.core.industrycore:1";
 
     private SubmodelFacadeWiremockSupport() {
     }
@@ -153,8 +149,7 @@ public final class SubmodelFacadeWiremockSupport {
                     "contractAgreementId": "%s",
                     "@context": %s
                 }
-                """.formatted(negotiationId, negotiationState, EDC_PROVIDER_DUMMY_URL, "BPNL00000001TEST",
-                contractAgreementId, CONTEXT);
+                """.formatted(negotiationId, negotiationState, EDC_PROVIDER_DUMMY_URL, "BPNL00000001TEST", contractAgreementId, CONTEXT);
     }
 
     private static String getTransferConfirmedResponse(final String transferProcessId, final String transferState,
@@ -182,8 +177,8 @@ public final class SubmodelFacadeWiremockSupport {
                     },
                     "@context": %s
                 }
-                """.formatted(transferProcessId, transferState, IRS_INTERNAL_CALLBACK_URL, transferProcessId,
-                edcAssetId, contractAgreementId, CONTEXT);
+                """.formatted(transferProcessId, transferState, IRS_INTERNAL_CALLBACK_URL, transferProcessId, edcAssetId, contractAgreementId,
+                CONTEXT);
     }
 
     @SuppressWarnings("PMD.UseObjectForClearerAPI") // used only for testing
@@ -240,9 +235,8 @@ public final class SubmodelFacadeWiremockSupport {
     }
 
     private static String createConstraints() {
-        final List<String> atomitConstraints = List.of(
-                createAtomicConstraint(CX_POLICY_FRAMEWORK_AGREEMENT, TRACEABILITY_1_0),
-                createAtomicConstraint(CX_POLICY_USAGE_PURPOSE, CX_CORE_INDUSTRYCORE_1));
+        final List<String> atomitConstraints = List.of(createAtomicConstraint("Membership", "active"),
+                createAtomicConstraint("FrameworkAgreement.traceability", "active"));
         return """
                 {
                   "odrl:and": [
