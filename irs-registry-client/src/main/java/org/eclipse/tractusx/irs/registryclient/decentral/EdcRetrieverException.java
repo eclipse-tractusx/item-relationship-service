a95e960ccc5b60b7ff898f436c6022f8dcb39dd6
Matthias Fischer <matthias.fischer@doubleslash.de>
2024-07-30 15:12:08 +0200

    chore(quality): [#841] add bpn and edcUrl attributes to EdcRetrieverException, add builder

--- irs-registry-client/src/main/java/org/eclipse/tractusx/irs/registryclient/decentral/EdcRetrieverException.java
+++ irs-registry-client/src/main/java/org/eclipse/tractusx/irs/registryclient/decentral/EdcRetrieverException.java
@@ -23,11 +23,54 @@
  ********************************************************************************/
 package org.eclipse.tractusx.irs.registryclient.decentral;
 
+import lombok.Getter;
+
 /**
  * Thrown in case of error in EDC communication
  */
+@Getter
 public class EdcRetrieverException extends Exception {
+
+    private String bpn;
+
+    private String edcUrl;
+
+    @Deprecated // TODO (mfischer) remove later, use the builder
     public EdcRetrieverException(final Throwable cause) {
         super(cause);
     }
+
+    private EdcRetrieverException(final Builder builder) {
+        super(builder.cause);
+        this.bpn = builder.bpn;
+        this.edcUrl = builder.edcUrl;
+    }
+
+    /**
+     * Builder for {@link EdcRetrieverException}
+     */
+    public static class Builder {
+        private final Throwable cause;
+        private String bpn;
+        private String edcUrl;
+
+        public Builder(final Throwable cause) {
+            this.cause = cause;
+        }
+
+        public Builder withBpn(final String bpn) {
+            this.bpn = bpn;
+            return this;
+        }
+
+        public Builder withEdcUrl(final String edcUrl) {
+            this.edcUrl = edcUrl;
+            return this;
+        }
+
+        public EdcRetrieverException build() {
+            return new EdcRetrieverException(this);
+        }
+    }
+
 }
